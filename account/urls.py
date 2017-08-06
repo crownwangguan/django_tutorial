11c2bbf308e05614073b226dc395a279c31e934f
crownwangguan <crown.wg@outlook.com>
2017-08-06 12:23:19 -0400

    user profile, add Pillow to upload image file. Also, register page and change password page

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # url(r'^login/$', views.user_login, name='login'),
     url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^register/$', views.register, name='register'),
     url(r'^login/$', django.contrib.auth.views.login, name='login'),
     url(r'^logout/$', django.contrib.auth.views.logout, name='logout'),
     url(r'^logout-then-login/$', django.contrib.auth.views.logout_then_login, name='logout_then_login'),
@@ -14,4 +15,5 @@ urlpatterns = [
     url(r'^password-reset/done/$', django.contrib.auth.views.password_reset_done, name='password_reset_done'),
     url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', django.contrib.auth.views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^password-reset/complete/$', django.contrib.auth.views.password_reset_complete, name='password_reset_complete'),
+    url(r'^edit/$', views.edit, name='edit'),
 ]
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -131,3 +131,6 @@ EMAIL_HOST_USER = 'asdf@gmail.com'
 EMAIL_HOST_PASSWORD = 'asdf'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
